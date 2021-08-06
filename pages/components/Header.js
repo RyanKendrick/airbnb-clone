7ac1521614576d0c575f9d64ab87216ba229a6a6
RyanKendrick <rpkendrick@gmail.com>
2021-08-06 15:21:56 -0400

    progress

--- pages/components/Header.js
+++ pages/components/Header.js
@@ -30,7 +30,7 @@ function Header() {
 
             {/* Right Div */}
             <div className='flex items-center space-x-4 justify-end text-gray-500'>
-                <p>Become a Host</p>
+                <p className='hidden md:inline cursor-pointer'>Become a Host</p>
                 <GlobeAltIcon className='h-6 cursor-pointer'/>
 
                 <div className='flex items-center space-x-2 border-2 p-2 rounded-full'>
